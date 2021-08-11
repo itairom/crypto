98912d8280ee25c47454ccb456aa376ec9488f69
itairom <iitaiirom@gmail.com>
2021-08-11 17:22:28 +0300

    finish active buttons

--- src/app/cmps/currency-details/currency-details.component.ts
+++ src/app/cmps/currency-details/currency-details.component.ts
@@ -13,8 +13,8 @@ import { AxiosService } from 'src/app/services/axios.service';
 export class CurrencyDetailsComponent implements OnInit {
   // @Input() state: any
   items: Currency = null
-  isTracked: Boolean = null
-
+  isTracked: boolean = null
+  activeBtn: string = null
 
   constructor(
     private axiosService: AxiosService
@@ -23,23 +23,29 @@ export class CurrencyDetailsComponent implements OnInit {
 
   }
 
+  setActiveBtn(status:string) {
+    console.log(status);
+    
+    this.activeBtn = status
+  }
+
   async setIsTracked() {
     this.isTracked = !this.isTracked
-    await this.axiosService.setIsTracked( this.isTracked,this.items.currency_id)
+    await this.axiosService.setIsTracked(this.isTracked, this.items.currency_id)
   }
 
   ngOnInit(): void {
     console.log(history.state.item);
-    
+
     if (history.state.item) {
       this.items = history.state.item
-      
+
       localStorage.setItem('currItem', JSON.stringify(history.state.item))
     }
     else {
       this.items = JSON.parse(localStorage.getItem('currItem'))
     }
-    
+
     this.isTracked = this.items?.is_tracked ? true : false
   }
 
